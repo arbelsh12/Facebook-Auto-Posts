66568afd14dc71c700d0acaff77c6f10fb07aa08
arbelsh12 <70585738+arbelsh12@users.noreply.github.com>
2022-05-13 13:58:31 +0300

    add Jokes API model

--- FacebookAutoPost/Program.cs
+++ FacebookAutoPost/Program.cs
@@ -13,6 +13,7 @@ namespace FacebookAutoPost
         private static PostingProvider _postingProvider;
         private static PostCreatingProvider _postCreatingProvider;
         private static ApplicationDbContext _context;
+        private static JokesAPI _jokesAPI;
 
         public static void Main(string[] args)
         {
@@ -20,14 +21,19 @@ namespace FacebookAutoPost
             _postingProvider = new PostingProvider();
             _postCreatingProvider = new PostCreatingProvider(_context);
 
+            // general post creation
             string pageID = "109056161633630";
             AutoPost user = _context.AutoPosts.Find(pageID);
             string pageUrl = "https://graph.facebook.com/109056161633630/feed";
-
             string postCotent = _postCreatingProvider.CreatePost(pageID).Result;
             var res = _postingProvider.postToPage(user.Token, pageUrl, postCotent).Result;
 
-            CreateHostBuilder(args).Build().Run();
+            //Joke post
+            _jokesAPI = new JokesAPI(_context);
+            string pageID2 = "107638691815379";
+            _jokesAPI.PostToPage(pageID2);
+
+          //  CreateHostBuilder(args).Build().Run();
         }
 
         //starts the web app
