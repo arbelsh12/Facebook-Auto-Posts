e4ff244acb51eef6114d46748710fc6021ca0700
arbelsh12 <70585738+arbelsh12@users.noreply.github.com>
2022-05-13 13:54:35 +0300

    add PageUrl property

--- FacebookAutoPost/Models/AutoPost.cs
+++ FacebookAutoPost/Models/AutoPost.cs
@@ -14,19 +14,29 @@ namespace FacebookAutoPost.Models
         [Key] // indicates that the first property is the primary key in the DB
         [DisplayName("Page ID")]
         public string PageId { get; set; }
+
         [Required]
         public string Token { get; set; }
+
         [DisplayName("User API")]
         [Required]
         public string UserAPI { get; set; }
+
         [DisplayName("Post Template")]
         public string PostTemplate { get; set; }
+
         public string Frequency { get; set; }
+
         public DateTime Time { get; set; }
 
         public string ApiKey { get; set; }
 
         public string Uri { get; set; }
+
+        public string PageUrl
+        {
+            get { return "https://graph.facebook.com/" + PageId +"/feed"; }
+        }
     }
 }
 
