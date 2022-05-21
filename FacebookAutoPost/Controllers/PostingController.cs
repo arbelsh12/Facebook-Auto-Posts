50914ba23419b95aecc36020de65ad89f2646a45
arbelsh12 <70585738+arbelsh12@users.noreply.github.com>
2022-05-21 19:53:15 +0300

    create buttons for auto post booking and news

--- FacebookAutoPost/Controllers/PostingController.cs
+++ FacebookAutoPost/Controllers/PostingController.cs
@@ -7,12 +7,16 @@ namespace FacebookAutoPost.Controllers
     public class PostingController : Controller
     {
         private readonly JokesAPI _jokesAPI;
+        private readonly BookingApi _bookingApi;
+        private readonly NewsApi _newsAPI;
         private readonly ApplicationDbContext _context;
 
         public PostingController(ApplicationDbContext context)
         {
             _context = context;
             _jokesAPI = new JokesAPI(_context);
+            _bookingApi = new BookingApi(_context);
+            _newsAPI = new NewsApi(_context);
         }
 
         public IActionResult Post()
@@ -24,6 +28,22 @@ namespace FacebookAutoPost.Controllers
         {
             string pageID = "107638691815379";
             _jokesAPI.PostToPage(pageID);
+
+            return View();
+        }
+
+        public IActionResult Booking()
+        {
+            string pageID = "109056161633630";
+            _bookingApi.postToPage(pageID);
+
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            string pageID = "105971235456078";
+            _bookingApi.postToPage(pageID);
 
             return View();
         }
