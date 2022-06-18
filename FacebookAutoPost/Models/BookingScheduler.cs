1c734c3824fe3a1d1a4e4bfc5f79b604ea6d8828
shanikrauss <70583407+shanikrauss@users.noreply.github.com>
2022-06-18 16:03:34 +0300

    clean Main, BookingScheduler - wip

--- FacebookAutoPost/Models/BookingScheduler.cs
+++ FacebookAutoPost/Models/BookingScheduler.cs
@@ -4,85 +4,14 @@ using System.Threading.Tasks;
 
 namespace FacebookAutoPost.Models
 {
-    public class BookingScheduler
+    public class BookingScheduler : Scheduler
     {
         StdSchedulerFactory factory;
 
-        public BookingScheduler()
-        {
-            // construct a scheduler factory using defaults
-            factory = new StdSchedulerFactory();
-        }
+        public BookingScheduler() : base()
+        { }
 
-        // can I make this func general?
-        // this template for
-        // 'schCron(string cron, string forJob, string jobGroup)'
-        // and
-        // 'getPostBookingJob(string forJob, string jobGroup)'
-        public async Task<int> schCron()
-        {
-            // get a scheduler
-            IScheduler scheduler = await factory.GetScheduler();
-            await scheduler.Start();
-
-            // define the job and tie it to our HelloJob class
-            IJobDetail job = JobBuilder.Create<PostBookingJob>()
-                .WithIdentity("postBooking", "group1")
-                .Build();
-
-            // Trigger the job to run now, and then every 40 seconds
-            ITrigger trigger = TriggerBuilder.Create()
-                .WithIdentity("myTrigger", "group1")
-                .WithCronSchedule("0 0/2 * * * ?")
-                .ForJob("postBooking", "group1")
-                .Build();
-
-            await scheduler.ScheduleJob(job, trigger);
-
-            return 1;
-        }
-
-        private async Task<int> schCron(IJobDetail job, string cron, string forJob, string jobGroup)
-        {
-            // get a scheduler
-            IScheduler scheduler = await factory.GetScheduler();
-            await scheduler.Start();
-
-            // define the job and tie it to our HelloJob class
-            //IJobDetail job = getPostBookingJob(forJob, jobGroup); // NEED await? 
-
-            // Trigger the job to run now, and then every 40 seconds
-            ITrigger trigger = TriggerBuilder.Create()
-                .WithIdentity("myTrigger", "group1")
-                .WithCronSchedule(cron)
-                .ForJob(forJob, jobGroup)
-                .Build(); // NEED await? 
-
-            await scheduler.ScheduleJob(job, trigger);
-
-            return 1;
-        }
-
-        public async Task<int> schCronJokes(string cron, string forJob, string jobGroup)
-        {
-            // define the job and tie it to our HelloJob class
-            IJobDetail job = getPostJokesJob(forJob, jobGroup); // NEED await? 
-
-            schCron(job, cron, forJob, jobGroup);
-
-            return 1;
-        }
-
-        public async Task<int> schCronNews(string cron, string forJob, string jobGroup)
-        {
-            // define the job and tie it to our HelloJob class
-            IJobDetail job = getPostNewsJob(forJob, jobGroup); // NEED await? 
-
-            schCron(job, cron, forJob, jobGroup);
-
-            return 1;
-        }
-
+      
         public async Task<int> schCronBooking(string cron, string forJob, string jobGroup)
         {
             // define the job and tie it to our HelloJob class
@@ -100,52 +29,6 @@ namespace FacebookAutoPost.Models
                 .Build();
 
             return job;
-        }
-
-        public IJobDetail getPostNewsJob(string forJob, string jobGroup)
-        {
-            IJobDetail job = JobBuilder.Create<PostNewsJob>()
-                .WithIdentity(forJob, jobGroup)
-                .Build();
-
-            return job;
-        }
-
-        public IJobDetail getPostJokesJob(string forJob, string jobGroup)
-        {
-            IJobDetail job = JobBuilder.Create<PostJokesJob>()
-                .WithIdentity(forJob, jobGroup)
-                .Build();
-
-            return job;
-        }
-
-        public async Task<int> sch()
-        {
-            // construct a scheduler factory using defaults
-            StdSchedulerFactory factory = new StdSchedulerFactory();
-
-            // get a scheduler
-            IScheduler scheduler = await factory.GetScheduler();
-            await scheduler.Start();
-
-            // define the job and tie it to our HelloJob class
-            IJobDetail job = JobBuilder.Create<PostBookingJob>()
-                .WithIdentity("myJob", "group1")
-                .Build();
-
-            // Trigger the job to run now, and then every 40 seconds
-            ITrigger trigger = TriggerBuilder.Create()
-                .WithIdentity("myTrigger", "group1")
-                .StartNow()
-                .WithSimpleSchedule(x => x
-                    .WithIntervalInSeconds(120)
-                    .WithRepeatCount(2))
-            .Build();
-
-            await scheduler.ScheduleJob(job, trigger);
-
-            return 1;
         }
 
     }
--- FacebookAutoPost/Program.cs
+++ FacebookAutoPost/Program.cs
@@ -22,22 +22,8 @@ namespace FacebookAutoPost
         {
             Scheduler scheduler = new Scheduler();
             scheduler.initScheduler();
-            scheduler.schCron<PostBookingJob>("0 0/2 * * * ?", "postBooking", "groupTEST", "triggerBooking");
-            scheduler.schCron<PostNewsJob>("0 0/2 * * * ?", "postNews", "groupTEST", "triggerNews");
-
-            //scheduler.schCronTEST("0 0/2 * * * ?", "postBooking", "groupTEST");
-
-            //BookingScheduler scheduler1 = new BookingScheduler();
-            //scheduler1.schCronBooking("0 0/2 * * * ?", "postBooking", "group1");
-            //var res2 = scheduler.schCronNews("0 /2 * * * ?", "postNews", "group2");
-
-            //NewsScheduler scheduler2 = new NewsScheduler();
-            //var res2 = scheduler2.schCronNews("0 0/2 * * * ?", "postNews", "group2");
-
-
-
-            //JokesScheduler jokesScheduler = new JokesScheduler();
-            //var res3 = jokesScheduler.schCronJokes("0 0/2 * * * ?", "postJokes", "group3");
+            scheduler.scheduleCronJob<PostBookingJob>("0 0/2 * * * ?", "postBooking", "groupTEST", "triggerBooking");
+            scheduler.scheduleCronJob<PostNewsJob>("0 0/2 * * * ?", "postNews", "groupTEST", "triggerNews");
 
 
             CreateHostBuilder(args).Build().Run();
