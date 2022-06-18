34d68c07ec6c49d351f7cad62597ee45dcc81ef2
shanikrauss <70583407+shanikrauss@users.noreply.github.com>
2022-06-18 15:53:58 +0300

    scheduler works for 2 pages (wip)

--- FacebookAutoPost/Models/Scheduler.cs
+++ FacebookAutoPost/Models/Scheduler.cs
@@ -1,12 +1,14 @@
 ﻿using Quartz;
 using Quartz.Impl;
+using System;
 using System.Threading.Tasks;
 
 namespace FacebookAutoPost.Models
 {
-    public abstract class Scheduler
+    public class Scheduler
     {
         StdSchedulerFactory factory;
+        IScheduler scheduler;
 
         public Scheduler()
         {
@@ -14,5 +16,151 @@ namespace FacebookAutoPost.Models
             factory = new StdSchedulerFactory();
         }
 
+        // can I make this func general?
+        // this template for
+        // 'schCron(string cron, string forJob, string jobGroup)'
+        // and
+        // 'getPostBookingJob(string forJob, string jobGroup)'
+
+
+        protected async Task<int> schCron(IJobDetail job, string cron, string forJob, string jobGroup)
+        {
+            // get a scheduler
+            IScheduler scheduler = await factory.GetScheduler();
+            await scheduler.Start();
+
+            // define the job and tie it to our HelloJob class
+            //IJobDetail job = getPostBookingJob(forJob, jobGroup); // NEED await? 
+
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "group1")
+                .WithCronSchedule(cron)
+                .ForJob(forJob, jobGroup)
+                .Build(); // NEED await? 
+
+            await scheduler.ScheduleJob(job, trigger);
+
+            return 1;
+        }
+
+        protected async Task<int> sch()
+        {
+            // construct a scheduler factory using defaults
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler scheduler = await factory.GetScheduler();
+            await scheduler.Start();
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<PostBookingJob>()
+                .WithIdentity("myJob", "group1")
+                .Build();
+
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(120)
+                    .WithRepeatCount(2))
+            .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+
+            return 1;
+        }
+
+        public async Task<int> schCron<T>(string cron, string jobName, string jobGroup, string triggerName) where T : IJob
+        {
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<T>()
+                  .WithIdentity(jobName, jobGroup)
+                  .Build();
+
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity(triggerName, jobGroup)
+                .WithCronSchedule(cron)
+                .Build();
+
+            try
+            {
+                await scheduler.ScheduleJob(job, trigger);
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+
+            return 1;
+        }
+
+        public async Task initScheduler()
+        {
+            // get a scheduler
+            scheduler = await factory.GetScheduler();
+            await scheduler.Start();
+
+        }
+
+
+
+        public async Task<int> schCronTEST(string cron, string forJob, string jobGroup)
+        {
+            // get a scheduler
+            IScheduler scheduler = await factory.GetScheduler();
+            //await scheduler.Start();
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<PostBookingJob>()
+                  .WithIdentity("postBooking", jobGroup)
+                  .Build();
+
+            IJobDetail job1 = JobBuilder.Create<PostNewsJob>()
+                .WithIdentity("postNews", jobGroup)
+                .Build();
+
+
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", jobGroup)
+                .WithCronSchedule(cron).Build(); // NEED await? 
+                //.ForJob(forJob, jobGroup)
+                //.Build(); // NEED await? 
+
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger1 = TriggerBuilder.Create()
+                .WithIdentity("myTrigger1", jobGroup)
+                .WithCronSchedule(cron).Build(); // NEED await?
+                //.ForJob(forJob, jobGroup)
+                //.Build(); // NEED await? 
+
+            await scheduler.Start();
+
+            try
+            {
+                await scheduler.ScheduleJob(job, trigger);
+                await scheduler.ScheduleJob(job1, trigger1);
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+
+            return 1;
+        }
+
+      
+
+        //public IJobDetail getJob<T>(string forJob, string jobGroup) where T : IJob
+        //{
+        //    IJobDetail job = JobBuilder.Create<T>()
+        //        .WithIdentity(forJob, jobGroup)
+        //        .Build();
+
+        //    return job;
+        //}
     }
 }
--- FacebookAutoPost/Program.cs
+++ FacebookAutoPost/Program.cs
@@ -1,18 +1,11 @@
-using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
 using FacebookAutoPost.Data;
 using FacebookAutoPost.Models;
-using System.Net.Http;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 /// <summary>
 //using NewsAPI;
 //using NewsAPI.Models;
-using NewsAPI.Constants;
-using System.Diagnostics;
-using Newtonsoft.Json.Linq;
-using System.Threading.Tasks;
 /// </summary>
 
 
@@ -27,56 +20,24 @@ namespace FacebookAutoPost
 
         public static void Main(string[] args)
         {
-           BookingScheduler scheduler = new BookingScheduler();
-           var res = scheduler.schCron();
+            Scheduler scheduler = new Scheduler();
+            scheduler.initScheduler();
+            scheduler.schCron<PostBookingJob>("0 0/2 * * * ?", "postBooking", "groupTEST", "triggerBooking");
+            scheduler.schCron<PostNewsJob>("0 0/2 * * * ?", "postNews", "groupTEST", "triggerNews");
+
+            //scheduler.schCronTEST("0 0/2 * * * ?", "postBooking", "groupTEST");
+
+            //BookingScheduler scheduler1 = new BookingScheduler();
+            //scheduler1.schCronBooking("0 0/2 * * * ?", "postBooking", "group1");
+            //var res2 = scheduler.schCronNews("0 /2 * * * ?", "postNews", "group2");
+
+            //NewsScheduler scheduler2 = new NewsScheduler();
+            //var res2 = scheduler2.schCronNews("0 0/2 * * * ?", "postNews", "group2");
 
 
 
-
-
-
-
-            //CreateHostBuilder(args).Build().Run();
-            _context = new ApplicationDbContext();
-
-            //BookingApi book = new BookingApi(_context);
-            //book.postToPage("109056161633630");
-
-            //NewsApi news = new NewsApi(_context);
-            //news.postToPage("105971235456078");
-
-            ////Joke post
-            //_jokesAPI = new JokesAPI(_context);
-            //string pageID2 = "107638691815379";
-            //_jokesAPI.PostToPage(pageID2);
-
-
-
-
-
-            //NewsApi n = new NewsApi();
-            //string post = n.getNewsTechArticle(Categories.Technology);
-            //_postingProvider = new PostingProvider();
-            //string pageID = "109056161633630";
-            //_context = new ApplicationDbContext();
-
-            //// general post creation
-            //string pageID = "109056161633630";
-            //AutoPost user = _context.AutoPosts.Find(pageID);
-            //string pageUrl = "https://graph.facebook.com/109056161633630/feed";
-            //var res = _postingProvider.postToPage(user.Token, pageUrl, post.Result).Result;
-
-
-            ////_postingProvider = new PostingProvider();
-            ////_postCreatingProvider = new PostCreatingProvider(_context);
-
-            ////string pageID = "109056161633630";
-            ////AutoPost user = _context.AutoPosts.Find(pageID);
-            ////string pageUrl = "https://graph.facebook.com/109056161633630/feed";
-
-            ////string postCotent = _postCreatingProvider.CreatePost(pageID).Result;
-            ////var res = _postingProvider.postToPage(user.Token, pageUrl, postCotent).Result;
-
+            //JokesScheduler jokesScheduler = new JokesScheduler();
+            //var res3 = jokesScheduler.schCronJokes("0 0/2 * * * ?", "postJokes", "group3");
 
 
             CreateHostBuilder(args).Build().Run();
