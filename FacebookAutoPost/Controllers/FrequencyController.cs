614647a13cf1b1ae7ea882b3b0d3e14e92ca1d48
shanikrauss <70583407+shanikrauss@users.noreply.github.com>
2022-07-23 15:47:08 +0300

    added view to Frequency table

---
+++ FacebookAutoPost/Controllers/FrequencyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using FacebookAutoPost.Data;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FacebookAutoPost.Controllers
+{
+    public class FrequencyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FrequencyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Frequency.ToListAsync());
+        }
+    }
+}
+
