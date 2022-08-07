110f6ea4193eb70aec9a20493ad80393f95585f1
arbelsh12 <70585738+arbelsh12@users.noreply.github.com>
2022-08-07 18:37:37 +0300

    remove Time column from the ParamsUri table

---
+++ FacebookAutoPost/Migrations/20220807151202_Remove Time Column.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FacebookAutoPost.Migrations
+{
+    public partial class RemoveTimeColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "AutoPosts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Time",
+                table: "AutoPosts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
