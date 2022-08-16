37b5bf3ec24e7643edf11448873fab2d82a0b69c
arbelsh12 <70585738+arbelsh12@users.noreply.github.com>
2022-08-16 10:37:23 +0300

    עןא דא

--- FacebookAutoPost/Controllers/ParamsUriController.cs
+++ FacebookAutoPost/Controllers/ParamsUriController.cs
@@ -19,5 +19,62 @@ namespace FacebookAutoPost.Controllers
         {
             return View(await _context.ParamsUri.ToListAsync());
         }
+
+        // GET: ParamsUri/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paramsUri = await _context.ParamsUri.FindAsync(id);
+
+            if (paramsUri == null)
+            {
+                return NotFound();
+            }
+            return View(paramsUri);
+        }
+
+        // POST: ParamsUri/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("PageId, ParamType1, ParamOne, RandomValue1, ParamType2, ParamTwo, RandomValue2, ParamType3, ParamThree, RandomValue3")] ParamsUri paramsUri)
+        {
+            if (id != paramsUri.PageId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(paramsUri);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ParamsUriExists(paramsUri.PageId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(paramsUri);
+        }
+
+        private bool ParamsUriExists(string id)
+        {
+            return _context.ParamsUri.Find(id) != null;
+        }
     }
 }
