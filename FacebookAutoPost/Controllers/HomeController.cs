a365ad5687b85296c95eb8b54de64d7227b35a09
shanikrauss <70583407+shanikrauss@users.noreply.github.com>
2022-09-18 19:48:05 +0300

    delete privacy

--- FacebookAutoPost/Controllers/HomeController.cs
+++ FacebookAutoPost/Controllers/HomeController.cs
@@ -21,10 +21,6 @@ namespace FacebookAutoPost.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
