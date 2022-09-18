c2fc0952aee598234fd79babbaed8a0f7fd434cd
shanikrauss <70583407+shanikrauss@users.noreply.github.com>
2022-09-18 19:46:57 +0300

    delete unnecery using

--- FacebookAutoPost/Controllers/HomeController.cs
+++ FacebookAutoPost/Controllers/HomeController.cs
@@ -2,10 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace FacebookAutoPost.Controllers
 {
